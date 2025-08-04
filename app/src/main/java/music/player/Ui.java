bf27d09b8c6c1d45a97d05120f105dfcc9ece6b1
Sudhan Shrestha <sudhanshrestha25@gmail.com>
2025-08-04 11:48:24 +0800

    added file for ui methods

---
+++ app/src/main/java/music/player/Ui.java
@@ -0,0 +1,36 @@
+package music.player;
+
+public class Ui {
+
+    public void printChoices() {
+        System.out.println("What would you like to do?");
+        System.out.println("1. Play from a playlist?");
+        System.out.println("2. Modify a playlist?");
+        System.out.println("3. Searchh for a track from library");
+    }
+
+}
+
+/*
+ * 
+ *      UI
+ *      *****************************************************************
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ *      *                                                               *
+ */
