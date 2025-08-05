3ffdc1a71fe82e5f631f22078e46201ff762afc0
Sudhan Shrestha <sudhanshrestha25@gmail.com>
2025-08-05 16:44:51 +0800

    Changing music player project to banking system

--- app/src/main/java/music/player/Ui.java
+++ app/src/main/java/music/player/Ui.java
@@ -2,35 +2,12 @@ package music.player;
 
 public class Ui {
 
-    public void printChoices() {
+    public static void printChoices() {
         System.out.println("What would you like to do?");
-        System.out.println("1. Play from a playlist?");
-        System.out.println("2. Modify a playlist?");
-        System.out.println("3. Searchh for a track from library");
+        System.out.println("1. Deposit Money");
+        System.out.println("2. Withdraw Money?");
+        System.out.println("3. Check your bank balance");
     }
 
 }
 
-/*
- * 
- *      UI
- *      *****************************************************************
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- *      *                                                               *
- */
