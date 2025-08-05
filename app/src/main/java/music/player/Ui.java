c58b86390d7879d4fba4952bf01db47b153b8839
Sudhan Shrestha <sudhanshrestha25@gmail.com>
2025-08-05 17:38:21 +0800

    Updated to take in account when intialised

--- app/src/main/java/music/player/Ui.java
+++ app/src/main/java/music/player/Ui.java
@@ -1,12 +1,19 @@
 package music.player;
 
 public class Ui {
-
+    private Account acc;
+    public Ui(Account acc) {
+        this.acc = acc;
+    }
     public static void printChoices() {
         System.out.println("What would you like to do?");
         System.out.println("1. Deposit Money");
         System.out.println("2. Withdraw Money?");
         System.out.println("3. Check your bank balance");
+    }
+
+    public static void showAccountDetails() {
+
     }
 
 }
